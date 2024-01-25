a54b7b6738aa72fb4a9ad3e7bc32ba3fc15b9f46
Bryar Osman <bryarosman.bo@gmail.com>
2024-01-25 09:52:53 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-bryaro-djangoblog-lmsissjvtku.ws-eu107.gitpod.io', '.herokuapp.com']
 
@@ -40,14 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
-    'blog',
     'django_summernote',
+    'blog',
+    'cloudinary',
     'about',
 ]
 
